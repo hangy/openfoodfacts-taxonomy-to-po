976d96cb222e3cc99e13a71bf6087172c4f94598
hangy <hangy@hangy.de>
2016-08-06 18:42:18 +0200

    Squashed '.openfoodfacts-lang-parser/' content from commit ab6eade
    git-subtree-dir: .openfoodfacts-lang-parser
    git-subtree-split: ab6eade987d1e162112c18b9c1bf844b3bcfed92

---
+++ OffLangParser/Culture.cs
@@ -0,0 +1,35 @@
+﻿namespace OffLangParser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+
+    public static class Culture
+    {
+        private static readonly IDictionary<string, CultureData> cultures = new Dictionary<string, CultureData>();
+
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Iso")]
+        /// <remarks>
+        /// http://stackoverflow.com/a/9841533/11963
+        /// </remarks>
+        public static CultureData FromIsoName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            var key = name.ToUpperInvariant();
+            CultureData result;
+            if (!cultures.TryGetValue(key, out result))
+            {
+                var info = new CultureInfo(name);
+                result = info != null ? new CultureData(info) : new CultureData(name);
+                cultures.Add(key, result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ OffLangParser/PrefixOnlyParser.cs
@@ -0,0 +1,34 @@
+﻿namespace OffLangParser
+{
+    using System;
+
+    public abstract class PrefixOnlyParser<TResult> : ISingleLineParser<TResult>
+    {
+        private readonly string prefix;
+
+        protected PrefixOnlyParser(string prefix)
+        {
+            this.prefix = string.IsNullOrWhiteSpace(prefix) ? string.Empty : string.Concat(prefix, Constants.PrefixSeparator);
+        }
+
+        public bool TryParse(string line, out TResult result)
+        {
+            result = default(TResult);
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                return false;
+            }
+
+            line = line.Trim();
+            if (!line.StartsWith(this.prefix, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            var lineWithoutPrefix = line.Substring(this.prefix.Length, line.Length - this.prefix.Length).Trim();
+            return this.TryParseWithoutPrefix(lineWithoutPrefix, out result);
+        }
+
+        protected abstract bool TryParseWithoutPrefix(string lineWithoutPrefix, out TResult result);
+    }
+}
